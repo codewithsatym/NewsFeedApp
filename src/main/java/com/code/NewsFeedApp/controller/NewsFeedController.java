3b727007f97d99f38cc8c662dfabaff13c7ee31f
saurabhkotwal <saurabhjobs92@gmail.com>
2024-05-11 18:31:16 +0530

    News Feed controller

--- src/main/java/com/code/NewsFeedApp/controller/NewsFeedController.java
+++ src/main/java/com/code/NewsFeedApp/controller/NewsFeedController.java
@@ -1,4 +1,19 @@
 package com.code.NewsFeedApp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Feed Apis")
 public class NewsFeedController {
+    @Operation(summary = "Feed api")
+    @PostMapping("/sdsds/(eventId)")
+    public ResponseEntity<?> postdd(@PathVariable Long eventId){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
