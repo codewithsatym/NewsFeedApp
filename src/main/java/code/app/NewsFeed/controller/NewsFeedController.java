d933a995d76a1ddc054a4f064626c8de68270a2b
saurabhkotwal <saurabhjobs92@gmail.com>
2024-05-12 15:51:01 +0530

    modified jpa not to create db

--- src/main/java/code/app/NewsFeed/controller/NewsFeedController.java
+++ src/main/java/code/app/NewsFeed/controller/NewsFeedController.java
@@ -5,18 +5,21 @@ import code.app.NewsFeed.dto.RequestEverythingDTO;
 import code.app.NewsFeed.utils.EverythingFeedHandler;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ValidationException;
 
 
 @RestController
+@RequestMapping(
+        produces = MediaType.APPLICATION_JSON_VALUE
+)
+@Tag(name = "OPEN SOURCE CALLING APIs")
 public class NewsFeedController {
 
     @Autowired
@@ -25,7 +28,7 @@ public class NewsFeedController {
 
     @Operation(summary = "Get EveryThing api")
     @GetMapping("/get-everything/{keyId}")
-    public ResponseEntity<JsonNode> sendEmailMessage(@PathVariable Long keyId, @RequestBody RequestEverythingDTO dto) throws ValidationException {
+    public ResponseEntity<JsonNode> getEveryThing(@PathVariable Long keyId, @RequestBody RequestEverythingDTO dto) throws ValidationException {
         JsonNode everyThingNewsFeed = everythingFeedHandler.getEveryThingNewsFeed(dto,keyId);
         return new ResponseEntity<>(everyThingNewsFeed,HttpStatus.OK);
     }
