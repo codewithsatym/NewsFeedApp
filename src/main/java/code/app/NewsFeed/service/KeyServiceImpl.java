fd0caeef18f5f7cb9eae8030057349aa870225a9
saurabhkotwal <saurabhjobs92@gmail.com>
2024-05-12 15:15:02 +0530

    modified jpa not to create db

--- src/main/java/code/app/NewsFeed/service/KeyServiceImpl.java
+++ src/main/java/code/app/NewsFeed/service/KeyServiceImpl.java
@@ -4,6 +4,7 @@ import code.app.NewsFeed.repository.KeyRepository;
 import code.app.NewsFeed.modal.KeysModal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class KeyServiceImpl implements KeyService {
@@ -11,10 +12,11 @@ public class KeyServiceImpl implements KeyService {
     private KeyRepository keyRepository;
 
     @Override
+    @Transactional
     public Long saveKey(String key) {
         KeysModal keysModal = new KeysModal();
         keysModal.setKey(key);
         keyRepository.saveAndFlush(keysModal);
-       return keysModal.getId();
+        return keysModal.getId();
     }
 }
--- src/main/resources/db/migration/V202405121318__CREATE_KEYS_ENTITY.sql
+++ src/main/resources/db/migration/V202405121318__CREATE_KEYS_ENTITY.sql
@@ -18,13 +18,13 @@ DECLARE
     v_hint text;
     v_context text;
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = v_schema_name_var) THEN
-        EXECUTE 'CREATE SCHEMA ' || v_schema_name_var;
-    END IF;
+   IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = v_schema_name_var) THEN
+           EXECUTE 'CREATE SCHEMA ' || v_schema_name_var;
+       END IF;
 
-    EXECUTE 'CREATE TABLE ' || v_schema_name_var || '.keys (
-        id SERIAL PRIMARY KEY,
-        created_date TIMESTAMP WITH TIME ZONE,
-        key_value VARCHAR(255) UNIQUE
-    )';
+       EXECUTE 'CREATE TABLE IF NOT EXISTS ' || v_schema_name_var || '.keys (
+           id SERIAL PRIMARY KEY,
+           created_date TIMESTAMP WITH TIME ZONE,
+           key_value VARCHAR(255) UNIQUE
+       )';
 END $$;
